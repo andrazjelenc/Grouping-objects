43c52f6b524afb2364e9d73d7865ce3948a62b33
andrazjelenc <andraz.jelenc@siol.net>
2016-09-12 19:32:55 +0200

    Update Main.java
    Bug: sqrt <> pow

--- Main.java
+++ Main.java
@@ -105,7 +105,7 @@ public class Main {
 							group = j;
 						}
 					}
-					localDistance += Math.sqrt(distance);
+					localDistance += Math.pow(distance,2);
 					groups.get(group).add(i);
 				}
 					
@@ -178,13 +178,15 @@ public class Main {
 	static void shuffleArray(int[] ar)
 	{
 		Random rnd = ThreadLocalRandom.current();
-	    	for (int i = ar.length - 1; i > 0; i--)
-	    	{
-	    		int index = rnd.nextInt(i + 1);
-	    		// Simple swap
-	    		int a = ar[index];
-	    		ar[index] = ar[i];
-	    		ar[i] = a;
-    		}
+	    for (int i = ar.length - 1; i > 0; i--)
+	    {
+	    	int index = rnd.nextInt(i + 1);
+	    	// Simple swap
+	    	int a = ar[index];
+	    	ar[index] = ar[i];
+	    	ar[i] = a;
+    	}
   	}
+	
+
 }
